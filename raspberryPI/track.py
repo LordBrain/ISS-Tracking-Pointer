629f58e28e6db62f86b34edec099e217be55d3cd
Brian Lee <brian.lee3@target.com>
2015-10-24 21:07:45 -0500

    Adding shutdown logic

--- raspberryPI/track.py
+++ raspberryPI/track.py
@@ -1,7 +1,7 @@
 import sys
 import math
 import time
-from main import *
+from __main__ import *
 
 #This will be where the tracking happens.
 #load location information
@@ -17,4 +17,4 @@ print("Site Lat Rad:")
 print(siteLatRad)
 print("Site Long Rad:")
 print(siteLonRad)
-print(main.spaceObjects[0])
+print(__main__.spaceObjects[0])
