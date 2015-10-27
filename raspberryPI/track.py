64ff62d7a2e66fde21af2b351d258a0b680189ed
Brian <brian@brianlee.org>
2015-10-26 20:53:09 -0500

    Cleaning up kill command

--- raspberryPI/track.py
+++ raspberryPI/track.py
@@ -10,52 +10,55 @@ execfile('location.py')
 # Object list
 execfile('objectList.py')
 
+# Kill command
+killTrack = shlex.split("ps ax | grep startTrack.py | grep -v grep | awk '{ print $1 }' | xargs kill -9")
+
 #arguments passed from main.
 trackObject = int(sys.argv[1])
 
 if(trackObject == 0):
     print("Track Object 0")
-    subprocess.Popen(['sudo','killall','startTrack.py'])
+    subprocess.Popen(killTrack)
     subprocess.Popen(['python','startTrack.py','0'])
 elif(trackObject == 1):
     print("Track Object 1")
-    subprocess.Popen(['sudo','killall','startTrack.py'])
+    subprocess.Popen(killTrack)
     subprocess.Popen(['python','startTrack.py','1'])
 elif(trackObject == 2):
     print("Track Object 2")
-    subprocess.Popen(['sudo','killall','startTrack.py'])
+    subprocess.Popen(killTrack)
     subprocess.Popen(['python','startTrack.py','2'])
 elif(trackObject == 3):
     print("Track Object 3")
-    subprocess.Popen(['sudo','killall','startTrack.py'])
+    subprocess.Popen(killTrack)
     subprocess.Popen(['python','startTrack.py','3'])
 elif(trackObject == 4):
     print("Track Object 4")
-    subprocess.Popen(['sudo','killall','startTrack.py'])
+    subprocess.Popen(killTrack)
     subprocess.Popen(['python','startTrack.py','4'])
 elif(trackObject == 5):
     print("Track Object 5")
-    subprocess.Popen(['sudo','killall','startTrack.py'])
+    subprocess.Popen(killTrack)
     subprocess.Popen(['python','startTrack.py','5'])
 elif(trackObject == 6):
     print("Track Object 6")
-    subprocess.Popen(['sudo','killall','startTrack.py'])
+    subprocess.Popen(killTrack)
     subprocess.Popen(['python','startTrack.py','6'])
 elif(trackObject == 7):
     print("Track Object 7")
-    subprocess.Popen(['sudo','killall','startTrack.py'])
+    subprocess.Popen(killTrack)
     subprocess.Popen(['python','startTrack.py','7'])
 elif(trackObject == 8):
     print("Track Object 8")
-    subprocess.Popen(['sudo','killall','startTrack.py'])
+    subprocess.Popen(killTrack)
     subprocess.Popen(['python','startTrack.py','8'])
 elif(trackObject == 9):
     print("Track Object 9")
-    subprocess.Popen(['sudo','killall','startTrack.py'])
+    subprocess.Popen(killTrack)
     subprocess.Popen(['python','startTrack.py','9'])
 elif(trackObject == 10):
     print("Track Object 10")
-    subprocess.Popen(['sudo','killall','startTrack.py'])
+    subprocess.Popen(killTrack)
     subprocess.Popen(['python','startTrack.py','10'])
 elif(trackObject == 11):
     print("Shutting down")
