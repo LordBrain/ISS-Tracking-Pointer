02f1e795dfc08fd8ce2b1a36fd58794f9e62cff1
Brian <brian@brianlee.org>
2015-10-26 21:09:20 -0500

    Cleaning up kill command

--- raspberryPI/track.py
+++ raspberryPI/track.py
@@ -19,47 +19,47 @@ trackObject = int(sys.argv[1])
 
 if(trackObject == 0):
     print("Track Object 0")
-    subprocess.Popen(killTrack,shell=True)
+    subprocess.Popen(['/bin/bash', '-c',killTrack])
     subprocess.Popen(['python','startTrack.py','0'])
 elif(trackObject == 1):
     print("Track Object 1")
-    subprocess.Popen(killTrack,shell=True)
+    subprocess.Popen(['/bin/bash', '-c',killTrack])
     subprocess.Popen(['python','startTrack.py','1'])
 elif(trackObject == 2):
     print("Track Object 2")
-    subprocess.Popen(killTrack,shell=True)
+    subprocess.Popen(['/bin/bash', '-c',killTrack])
     subprocess.Popen(['python','startTrack.py','2'])
 elif(trackObject == 3):
     print("Track Object 3")
-    subprocess.Popen(killTrack,shell=True)
+    subprocess.Popen(['/bin/bash', '-c',killTrack])
     subprocess.Popen(['python','startTrack.py','3'])
 elif(trackObject == 4):
     print("Track Object 4")
-    subprocess.Popen(killTrack,shell=True)
+    subprocess.Popen(['/bin/bash', '-c',killTrack])
     subprocess.Popen(['python','startTrack.py','4'])
 elif(trackObject == 5):
     print("Track Object 5")
-    subprocess.Popen(killTrack,shell=True)
+    subprocess.Popen(['/bin/bash', '-c',killTrack])
     subprocess.Popen(['python','startTrack.py','5'])
 elif(trackObject == 6):
     print("Track Object 6")
-    subprocess.Popen(killTrack,shell=True)
+    subprocess.Popen(['/bin/bash', '-c',killTrack])
     subprocess.Popen(['python','startTrack.py','6'])
 elif(trackObject == 7):
     print("Track Object 7")
-    subprocess.Popen(killTrack,shell=True)
+    subprocess.Popen(['/bin/bash', '-c',killTrack])
     subprocess.Popen(['python','startTrack.py','7'])
 elif(trackObject == 8):
     print("Track Object 8")
-    subprocess.Popen(killTrack,shell=True)
+    subprocess.Popen(['/bin/bash', '-c',killTrack])
     subprocess.Popen(['python','startTrack.py','8'])
 elif(trackObject == 9):
     print("Track Object 9")
-    subprocess.Popen(killTrack,shell=True)
+    subprocess.Popen(['/bin/bash', '-c',killTrack])
     subprocess.Popen(['python','startTrack.py','9'])
 elif(trackObject == 10):
     print("Track Object 10")
-    subprocess.Popen(killTrack,shell=True)
+    subprocess.Popen(['/bin/bash', '-c',killTrack])
     subprocess.Popen(['python','startTrack.py','10'])
 elif(trackObject == 11):
     print("Shutting down")
