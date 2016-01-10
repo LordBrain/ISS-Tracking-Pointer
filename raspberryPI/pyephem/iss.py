0b9c95fee46b92bad6e26527860f39a52ef4f75d
Brian <brian@brianlee.org>
2016-01-10 13:32:32 -0600

    update

--- raspberryPI/pyephem/iss.py
+++ raspberryPI/pyephem/iss.py
@@ -23,30 +23,8 @@ f.close()
 for i, line in enumerate(searchlines):
     if "ISS" in line:
         for l in searchlines[i:i+3]: space_array.append(l)
-        #     exec("l%d = %s" % (i, l),
-        # print("l%d" % (i)),
-        # print
 
-print(space_array[0])
-print(space_array[1])
-print(space_array[2])
-# print("l1: %s" % (l[0]))
-# time.strftime("%Y/%m/%d %H:%M:%S")
-
-# Load Satellite TLE data.
-# l1 = 'ISS (ZARYA)'
-# l2 = '1 25544U 98067A   15362.57617266  .00007229  00000-0  11208-3 0  9992'
-# l3 = '2 25544  51.6430 193.9420 0008214 334.7441 131.3770 15.55152352978280'
 sat = ephem.readtle(space_array[0],space_array[1],space_array[2])
 sat.compute(location)
-print("%s;%s" % (sat.alt / ephem.degree, sat.az / ephem.degree))
-
-# body = ephem.Mars(location)
 # output Alt;AZ
-# print("%s;%s" % (body.alt / ephem.degree, body.az / ephem.degree))
-# print("AZ: %s" % (body.az / ephem.degree))
-
-# print("Sat")
-# print("AZ Deg: %s" % (sat.az / ephem.degree))
-# print("ALT Deg: %s" % (sat.alt / ephem.degree))
-# print("")
+print("%s;%s" % (sat.alt / ephem.degree, sat.az / ephem.degree))
