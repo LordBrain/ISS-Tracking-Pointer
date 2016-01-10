67ff7860a8feb69779b38e13ff400604672102b7
Brian <brian@brianlee.org>
2016-01-10 13:02:27 -0600

    update

--- raspberryPI/pyephem/iss.py
+++ raspberryPI/pyephem/iss.py
@@ -22,7 +22,8 @@ searchlines = f.readlines()
 f.close()
 for i, line in enumerate(searchlines):
     if "ISS" in line:
-        for l in searchlines[i:i+3]: line.append(l)
+        for l in searchlines[i:i+3]: print l,
+        print
         #     exec("l%d = %s" % (i, l),
         # print("l%d" % (i)),
         # print
